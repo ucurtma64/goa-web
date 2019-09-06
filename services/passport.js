105362f5789d4b7bea25fa2cc85c029adfa2be33
Lix3nn53 <lixakyildiz@gmail.com>
2019-09-07 01:34:20 +0300

    react , redux, authReducer setup. Materialize-css. FetchUser

--- services/passport.js
+++ services/passport.js
@@ -22,6 +22,27 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id });
+    
+        if (existingUser) return done(null, existingUser); //we already have a record with given profile.id
+
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user); //call done after saving user(async db call) is completed
+    }
+    )
+);
+
+/*
+Before refactoring using Async/Await
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true
     }, (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id })
                 .then((existingUser) => {
@@ -36,4 +57,4 @@ passport.use(
                 });
         }
     )
-);
+);*/
