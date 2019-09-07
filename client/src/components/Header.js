34f41f27bfd8fd93c7b5e6fefffb938c5a4cd320
Lix3nn53 <lixakyildiz@gmail.com>
2019-09-07 22:05:34 +0300

    Test heroku deployment

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
             case false:
                 return <li><a href="/auth/google">Login with google</a></li>;
             default:
-                    return <li><a href="/api/logout">Logout</a></li>;
+                return <li><a href="/api/logout">Logout</a></li>;
         }
     }
 
