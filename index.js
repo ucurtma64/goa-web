ce5da9938c69c164b504e593a21d685a5763e042
Lix3nn53 <lixakyildiz@gmail.com>
2019-09-09 11:45:15 +0300

    Handle payments via Stripe

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const path = require('path');
 require('./models/User'); //add use schema before using it in ./services/passport.js
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
