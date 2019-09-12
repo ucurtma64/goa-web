4e5d1d4b23a1e41fbea2de13c4c2fb651b658f23
Lix3nn53 <lixakyildiz@gmail.com>
2019-09-12 13:26:46 +0300

    Back-end survey creation. Using sendgrid to send emails but does not work.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User'); //add use schema before using it in ./services/passport.js
+require('./models/Survey');
 require('./services/passport'); //not assigned to a variable since we need this to run only once
 
 mongoose.connect(keys.mongoURI);
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 const path = require('path');
 //Express will serve up production assets like main.css and main.js files
