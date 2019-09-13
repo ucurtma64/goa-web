aa82f609cf97ebd9b50c7475a8fe0587ea87fb05
Lix3nn53 <lixakyildiz@gmail.com>
2019-09-13 20:12:20 +0300

    Update to new sendgrid package. The package used in the course has been deprecated.

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,12 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+/*
+developer only
+
 import axios from 'axios';
 window.axios = axios;
+*/
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,53 +1,36 @@
-const sendgrid = require('sendgrid');
+const sendgrid = require('@sendgrid/mail');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {
+class Mailer {
     constructor({ subject, recipients }, content) {
-        super();
+        sendgrid.setApiKey(keys.sendgridKey);
 
-        this.sgApi = sendgrid(keys.sendgridKey);
-        this.from_email = new helper.Email('no-reply@emaily.com');
-        this.subject = subject;
-        this.body = new helper.Content('text/html', content);
-        this.recipients = this.formatAddresses(recipients);
+        const emails = recipients.map(({ email }) => email);
 
-        this.addContent(this.body);
-        this.addClickTracking();
-        this.addRecipients();
-    }
-
-    formatAddresses(recipients) {
-        return recipients.map(({ email }) => {
-          return new helper.Email(email);
-        });
-    }
-
-    addClickTracking() {
-        const trackingSettings = new helper.TrackingSettings();
-        const clickTracking = new helper.ClickTracking(true, true);
-    
-        trackingSettings.setClickTracking(clickTracking);
-        this.addTrackingSettings(trackingSettings);
-    }
-
-    addRecipients() {
-        const personalize = new helper.Personalization();
-    
-        this.recipients.forEach(recipient => {
-          personalize.addTo(recipient);
-        });
-        this.addPersonalization(personalize);
+        this.msg = {
+            to: emails,
+            from: 'no-reply@emaily.com',
+            subject: subject,
+            text: 'Hello plain world!',
+            html: content,
+            trackingSettings: {
+                "clickTracking": {
+                    "enable": true
+                },
+                "openTracking": {
+                    "enable": false
+                },
+                "subscriptionTracking": {
+                    "enable": false
+                }
+            }
+        };
     }
 
     async send() {
-        const request = this.sgApi.emptyRequest({
-            method: 'POST',
-            path: '/v3/mail/send',
-            body: this.toJSON()
-        });
+        const response = await sendgrid.sendMultiple(this.msg);
 
-        const response = await this.sgApi.API(request);
         return response;
     }
 }
