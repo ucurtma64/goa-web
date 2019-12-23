007ab5bb51a593769a71624f58e3f4f457d8714b
Lix3nn53 <lixakyildiz@gmail.com>
2019-12-23 11:59:03 +0300

    Make stepper responsive

--- client/src/components/util/HorizontalStepper.js
+++ client/src/components/util/HorizontalStepper.js
@@ -42,7 +42,7 @@ class HorizontalStepper extends Component {
     for (var i = 0; i < this.props.stages.length; i++) {
       if (i < this.state.currentStage) {
         steps[i] = (
-          <div key={i} className="mdl-stepper-step step-done">
+          <div key={i} className="col mdl-stepper-step step-done">
             <div className="mdl-stepper-circle">
               <span>{i + 1}</span>
             </div>
@@ -62,7 +62,7 @@ class HorizontalStepper extends Component {
         );
       } else if (i > this.state.currentStage) {
         steps[i] = (
-          <div key={i} className="mdl-stepper-step">
+          <div key={i} className="col mdl-stepper-step">
             <div className="mdl-stepper-circle">
               <span>{i + 1}</span>
             </div>
@@ -82,7 +82,7 @@ class HorizontalStepper extends Component {
         );
       } else {
         steps[i] = (
-          <div key={i} className="mdl-stepper-step active-step">
+          <div key={i} className="col mdl-stepper-step active-step">
             <div className="mdl-stepper-circle">
               <span>{i + 1}</span>
             </div>
@@ -108,156 +108,13 @@ class HorizontalStepper extends Component {
 
   render() {
     return (
-      <>
-        <style type="text/css">
-          {`
-                .mdl-card {
-                  min-height: 0;
-                  margin: 10px auto;
-                }
-                
-                .mdl-card__supporting-text {
-                  width: 100%;
-                  padding: 0;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step {
-                  width: 25%;
-                  /* 100 / no_of_steps */
-                }
-                
-                
-                /* Begin actual mdl-stepper css styles */
-                
-                .mdl-stepper-horizontal-alternative {
-                  display: table;
-                  width: 100%;
-                  margin: 0 auto;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step {
-                  display: table-cell;
-                  position: relative;
-                  padding: 1.6rem;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:hover,
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:active {
-                  background-color: rgba(255, 255, 255, .06);
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:active {
-                  border-radius: 15% / 75%;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:first-child:active {
-                  border-top-left-radius: 0;
-                  border-bottom-left-radius: 0;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:last-child:active {
-                  border-top-right-radius: 0;
-                  border-bottom-right-radius: 0;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:hover .mdl-stepper-circle {
-                  background-color: #757575;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:first-child .mdl-stepper-bar-left,
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step:last-child .mdl-stepper-bar-right {
-                  display: none;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-circle {
-                  width: 3rem;
-                  height: 3rem;
-                  margin: 0 auto;
-                  background-color: #90a4ae;
-                  border-radius: 50%;
-                  text-align: center;
-                  line-height: 2em;
-                  font-size: 1.5rem;
-                  color: white;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step.active-step .mdl-stepper-circle {
-                  background-color: #e2b11d;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step.step-done .mdl-stepper-circle {
-                    background-color:  #7ab92d;
-                }
-
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step.editable-step .mdl-stepper-circle {
-                    background-color: #90a4ae;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step.editable-step .mdl-stepper-circle {
-                  -moz-transform: scaleX(-1);
-                  /* Gecko */
-                  -o-transform: scaleX(-1);
-                  /* Opera */
-                  -webkit-transform: scaleX(-1);
-                  /* Webkit */
-                  transform: scaleX(-1);
-                  /* Standard */
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-title {
-                  margin-top: 0.1rem;
-                  font-size: 1.4rem;
-                  font-weight: normal;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-title,
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-optional {
-                  text-align: center;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step.active-step .mdl-stepper-title {
-                  font-weight: 500;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step.active-step.step-done .mdl-stepper-title,
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step.active-step.editable-step .mdl-stepper-title {
-                  font-weight: 300;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-optional {
-                  font-size: 1rem;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-bar-left,
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-bar-right {
-                  position: absolute;
-                  top: 3rem;
-                  height: 1px;
-                  border-top: 1.5px solid #000000;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-bar-right {
-                  right: 0;
-                  left: 55%;
-                  margin-left: 20px;
-                }
-                
-                .mdl-stepper-horizontal-alternative .mdl-stepper-step .mdl-stepper-bar-left {
-                  left: 0;
-                  right: 55%;
-                  margin-right: 20px;
-                }
-                `}
-        </style>
-
-        <div className="mdl-card mdl-shadow--2dp">
-          <div className="mdl-card__supporting-text">
-            <div className="mdl-stepper-horizontal-alternative">
-              {this.renderStages()}
-            </div>
+      <div className="container">
+        <div className="mdl-card__supporting-text">
+          <div className="row mdl-stepper-horizontal-alternative">
+            {this.renderStages()}
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
